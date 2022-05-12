47dd51d8c5ec59405f3b76a8cb9440c2b3ace3a6
chunqiangqian <chunqiang_qian@163.com>
2022-05-12 16:15:47 +0800

    补充漏掉的字符串分割函数

--- lib/scan.cpp
+++ lib/scan.cpp
@@ -108,6 +108,25 @@ int usb_camera_open(const char *dev, v4l2_buf** mV4l2Buf, std::string &card)
 
 }
 
+void Stringsplit(std::string str, char split, std::vector<std::string>& res)
+{
+	if (str == "")		return;
+	//在字符串末尾也加入分隔符，方便截取最后一段
+	std::string strs = str + split;
+	size_t pos = strs.find(split);
+ 
+	// 若找不到内容则字符串搜索函数返回 npos
+	while (pos != strs.npos)
+	{
+		std::string temp = strs.substr(0, pos);
+		res.push_back(temp);
+		//去掉已分割的字符串,在剩下的字符串中进行分割
+		strs = strs.substr(pos + 1, strs.size());
+		pos = strs.find(split);
+	}
+}
+
+
 
 void ScanCameras(std::vector<std::string> &camCards, std::vector<std::string> &camPaths)
 {
